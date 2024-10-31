bb321f40842af61a4fa288e23140330504efdf19
ademmelki <80390107+Ademmelki01@users.noreply.github.com>
2024-10-31 18:49:23 +0100

    Delete src/main/java/tn/esprit/tpfoyer/repository/UserRepository.java

--- src/main/java/tn/esprit/tpfoyer/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.tpfoyer.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.tpfoyer.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Vous pouvez ajouter des méthodes supplémentaires si nécessaire
-}
